95030dfddaa5568066327dab9529381dace5f2cd
alvarovilaplanagarcia <avilaplana@equalexperts.com>
2013-04-01 20:59:30 +0100

    exercise 29 and fixing SquareCalculator implemented in 16

--- src/main/scala/euler/scala/exercise16/SquareCalculator.scala
+++ src/main/scala/euler/scala/exercise16/SquareCalculator.scala
@@ -7,7 +7,7 @@ class SquareCalculator {
   def square(term: Int, square : Int) : BigInt = {
 
     var product = BigInt(1)
-    for (mult <- 1 to square) product = product * 2
+    for (mult <- 1 to square) product = product * term
     product
   }
 
