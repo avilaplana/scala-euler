58b70ed280c0cf412a2d8b67e31238f5bd52dc6e
alvarovilaplanagarcia <avilaplana@equalexperts.com>
2014-05-09 10:12:30 +0100

    ejercicio

--- src/main/scala/euler/scala/change/CountingChange.scala
+++ src/main/scala/euler/scala/change/CountingChange.scala
@@ -3,4 +3,15 @@ package euler.scala.change
 
 class CountingChange {
 
+  def countChange(money: Int, coins: List[Int], partialSolution: List[Int] = List()): Int = {
+    coins.map {
+      coin => (money - coin) match {
+        case 0 => println(s"solution ${partialSolution :+ coin}")
+        case someNumber => if (someNumber > 0) countChange(someNumber, coins, partialSolution :+ coin)
+        case someNumber => if (someNumber < 0) return 0
+      }
+    }
+    return 0
+  }
+
 }
--- src/test/scala/euler/scala/change/CountingChangeSpecs.scala
+++ src/test/scala/euler/scala/change/CountingChangeSpecs.scala
@@ -1,6 +1,16 @@
 package euler.scala.change
 
+import org.specs2.mutable.Specification
+import euler.scala.parenthesesBalancing.Balancing
 
-class CountingChangeSpecs {
+
+class CountingChangeSpecs extends Specification {
+
+  "number of change for 4" should {
+    "be true" in {
+      new CountingChange().countChange(4,List(1,2)) must_==(0)
+    }
+  }
+
 
 }
