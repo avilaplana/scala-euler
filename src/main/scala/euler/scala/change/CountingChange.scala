173a91761dfe7e025198b3d85f14011c8b7a9d9d
alvarovilaplanagarcia <avilaplana@equalexperts.com>
2014-05-11 21:26:25 +0100

    finish the solution of calculating change

--- src/main/scala/euler/scala/change/CountingChange.scala
+++ src/main/scala/euler/scala/change/CountingChange.scala
@@ -1,17 +1,30 @@
 package euler.scala.change
 
+import collection.mutable.{LinkedHashSet, Set}
 
 class CountingChange {
 
+  val solutionsCombinations: LinkedHashSet[List[Int]] = new LinkedHashSet()
+
   def countChange(money: Int, coins: List[Int], partialSolution: List[Int] = List()): Int = {
-    coins.map {
+    val result = coins.map {
       coin => (money - coin) match {
-        case 0 => println(s"solution ${partialSolution :+ coin}")
-        case someNumber => if (someNumber > 0) countChange(someNumber, coins, partialSolution :+ coin)
-        case someNumber => if (someNumber < 0) return 0
+        case 0 => {
+          println(s"solution ${partialSolution :+ coin}")
+          isSolutionProcessed(solutionsCombinations, partialSolution :+ coin) match {
+            case true => 0
+            case false => solutionsCombinations.add(partialSolution :+ coin); 1
+          }
+        }
+        case someNumber if (someNumber > 0) => countChange(someNumber, coins, partialSolution :+ coin)
+        case someNumber if (someNumber < 0) => 0
       }
     }
-    return 0
+    result.sum
+  }
+
+  private def isSolutionProcessed(solutions: Set[List[Int]], current: List[Int]) = {
+    solutions.exists(_.sorted == current.sorted)
   }
 
 }
--- src/test/scala/euler/scala/change/CountingChangeSpecs.scala
+++ src/test/scala/euler/scala/change/CountingChangeSpecs.scala
@@ -6,11 +6,16 @@ import euler.scala.parenthesesBalancing.Balancing
 
 class CountingChangeSpecs extends Specification {
 
-  "number of change for 4" should {
-    "be true" in {
-      new CountingChange().countChange(4,List(1,2)) must_==(0)
+  "number of change for 4 with coins with denomination 1 and 2" should {
+    "be 3" in {
+      new CountingChange().countChange(4,List(1,2)) must_==(3)
     }
   }
 
+  "number of change for 5 with coins with denomination 1 and 2" should {
+      "be 3" in {
+        new CountingChange().countChange(5,List(1,2)) must_==(3)
+      }
+    }
 
 }
